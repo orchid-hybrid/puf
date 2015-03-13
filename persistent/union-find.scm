8f8b3a11478e1982a872d2e52fbe0fcc1057706e
tca <theconartist@gmail.com>
2015-03-13 16:56:09 -0500

    union

--- persistent/union-find.scm
+++ persistent/union-find.scm
@@ -15,3 +15,20 @@
   (let-values (((f cx) (find-aux (puf-father h) x)))
     (puf-set-father! h f)
     cx))
+
+(define (union h x y)
+  (let ((rx (find h x))
+        (ry (find h y)))
+    (if (= rx ry)
+        h
+        (let ((father (puf-father h))
+              (c (puf-c h)))
+        (let ((rxc (parray-get c rx))
+              (ryc (parray-get c ry)))
+          (cond ((> rxc ryc)
+                 (puf (parray-set father c ry rx) c))
+                ((< rxc ryc)
+                 (puf (parray-set father c rx ry) c))
+                (else 
+                 (puf (parray-set father ry rx)
+                      (parray-set c rx (+ rxc 1))))))))))
